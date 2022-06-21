4447a1c960b00a65f05c45defdf4c751d11c481a
mariasantosdev <mariaeduardasrodolfo2@gmail.com>
2022-06-20 22:52:31 -0300

    alterando mensagem

--- src/Inscrito.java
+++ src/Inscrito.java
@@ -1,7 +1,7 @@
 public class Inscrito implements Observador, AcoesDoInscrito {
     @Override
     public void assistirVideo() {
-        System.out.println("Video assistido");
+        System.out.println("Acabou de chegar uma notificação por sininho");
     }
 
     @Override
